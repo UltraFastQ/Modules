7e8d621ad8c2c7ca67bf97c414d49c21a2ad3c7b
Nicolas Perron <nicolas-2.perron@polymtl.ca>
2019-08-21 09:49:59 -0400

    Modified for pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="femtoMod",
-    version="1.0.2",
+    version="1.0.3",
     author="Patrick, Benjamin, Etienne",
     description="femtoQ Modules",
     long_description=long_description,
