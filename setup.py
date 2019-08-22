5a43eb6d11d23fc5702e15f10004e90d46df2b7e
Etdoi <etienne_doiron@hotmail.com>
2019-08-22 18:43:41 -0400

    Updated pip version to match latest commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="femtoMod",
-    version="1.0.7",
+    version="1.1.0",
     author="Patrick, Benjamin, Etienne",
     description="femtoQ Modules",
     long_description=long_description,
