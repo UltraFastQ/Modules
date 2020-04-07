a65d747d8c66f756afe9e7dbc7c68f99e76fe3bd
Etdoi <etienne_doiron@hotmail.com>
2020-04-07 14:04:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="femtoMod",
-    version="1.3.3",
+    version="1.4.0",
     author="Patrick, Benjamin, Etienne",
     description="femtoQ Modules",
     long_description=long_description,
