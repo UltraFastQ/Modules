a811bbbdca997b3e21247ea5e436d3da3159ba6d
UltraFastQ Lab <40898165+UltraFastQ@users.noreply.github.com>
2022-06-09 10:25:22 -0400

    updated pip version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="femtoMod",
-    version="3.2.4",
+    version="3.2.5",
     author="Patrick, Benjamin, Etienne",
     description="femtoQ Modules",
     long_description=long_description,
